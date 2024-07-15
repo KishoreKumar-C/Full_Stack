de96a2fbdadd92fcf616babdbcd1260f273f2191
Kishore-04012003 <kishorelingan@gmail.com>
2024-07-15 15:26:56 +0530

    File updated

--- 07_JS_Object_Arguments/Ch7_pgm_04.js
+++ 07_JS_Object_Arguments/Ch7_pgm_04.js
@@ -1,10 +1,14 @@
 // Moving a point in 2D
 
 var point1;
-var point2;
+var point2, point3, point4;
 var move;
 var showPoint;
-var reflectX;
+var reflectX, reflectY, linBreak;
+
+function linBreak() {
+    console.log("-------------------------------------------");
+}
 
 move = function (point, change) {
     return {
@@ -13,13 +17,23 @@ move = function (point, change) {
     };
 };
 
-reflectX = function(point, change) {
+reflectX = function(point) {
     return{
-        x: point.x + change.x,
-        y: point.y
+        x : point.x,
+        y: -point.y
     }
 
 }
+
+reflectY = function(point) {
+    return{
+        x : -point.x,
+        y : point.y
+    }
+
+}
+
+
 
 showPoint = function (point) {
     console.log("( " + point.x + " , " + point.y + " )");
@@ -29,17 +43,27 @@ point1 = { x : 2, y : 5 };
 
 point2 = move(point1, { x : 7, y : -4 });
 
-point3 = reflectX(point1, {x : 10 , y : 4});
+point3 = reflectX(point1);
 
+point4 = reflectY(point1);
 
 showPoint(point1);
 console.log("Move 7 across and 4 down");
 showPoint(point2);
 
+linBreak();
+
 showPoint(point1);
 console.log("After the reflection in X axis");
-showPoint(point3)
+showPoint(point3);
 
+linBreak();
+
+showPoint(point1);
+console.log("After the reflection in Y axis");
+showPoint(point4);
+
+linBreak();
 
 
 /* Further Adventures
